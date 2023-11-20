871e8ca4f84ac571901a8cfc298b37e095b07902
Julia Gorbatenko <ellis.tom@internet.ru>
2023-11-20 13:02:07 +0300

    task2

---
+++ onditional operators and loops/task2/task2.js
@@ -0,0 +1,26 @@
+
+function testNum(x,y,z){
+    let x=3;
+    let y=-7;
+    let z=2;
+if (x>0 && y>0 && z>0)
+{
+       console.log("Знак +");
+}
+else if (x<0 && y<0 && z<0)
+        {
+          console.log("Знак +");
+        }
+        else if (x>0 && y<0 && z<0)
+        {
+          console.log("Знак +");
+        }
+        else if (x<0 && y>0 && z<0)
+        {
+          console.log("Знак +");
+        }
+        else
+        {
+          console.log("Знак -");
+        }
+    }
